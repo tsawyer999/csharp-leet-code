cca9808b7db94bfc21a97e7b1c65dec5bdf93997
name <email>
2023-09-22 16:26:32 -0400

    features: implement sum two strings

---
+++ Exercices.UnitTests/SumTwoStringsTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Exercices.UnitTests;
+
+[TestClass]
+public class SumTwoStringsTests
+{
+    [TestMethod]
+    [DataRow("a", "10")]
+    [DataRow("-", "20")]
+    [DataRow(".", "30")]
+    [DataRow("40", "+")]
+    [DataRow("50", "@")]
+    public void WhenValueIsNotNumericThenReturnMinusOne(string a, string b)
+    {
+        var sum = SumTwoStrings.SumStrings(a, b);
+
+        Assert.AreEqual("-1", sum);
+    }
+
+    [TestMethod]
+    [DataRow("", "1", "1")]
+    [DataRow("2", "", "2")]
+    [DataRow("", "", "0")]
+    public void WhenValueIsEmptyConsiderZero(string a, string b, string expectedResult)
+    {
+        var sum = SumTwoStrings.SumStrings(a, b);
+
+        Assert.AreEqual(expectedResult, sum);
+    }
+
+    [TestMethod]
+    public void SumTwoNumbersWithSameLength()
+    {
+        var sum = SumTwoStrings.SumStrings("1", "2");
+
+        Assert.AreEqual("3", sum);
+    }
+
+    [TestMethod]
+    public void SumTwoNumbersWithPaddedZero()
+    {
+        var sum = SumTwoStrings.SumStrings("0001", "0002");
+
+        Assert.AreEqual("3", sum);
+    }
+    
+    [TestMethod]
+    public void SumTwoNumbersWithARetainer()
+    {
+        var sum = SumTwoStrings.SumStrings("7", "5");
+
+        Assert.AreEqual("12", sum);
+    }
+
+    [TestMethod]
+    [DataRow("2", "14", "16")]
+    [DataRow("99", "2", "101")]
+    [DataRow("10000", "5", "10005")]
+    public void SumTwoNumbersWithVariousLength(string a, string b, string expectedResult)
+    {
+        var sum = SumTwoStrings.SumStrings(a, b);
+
+        Assert.AreEqual(expectedResult, sum);
+    }
+
+    [TestMethod]
+    public void SumTwoLargeNumbers()
+    {
+        var sum = SumTwoStrings.SumStrings("712569312664357328695151392", "8100824045303269669937");
+
+        Assert.AreEqual("712577413488402631964821329", sum);
+    }
+}
---
+++ Exercices/SumTwoStrings.cs
@@ -0,0 +1,70 @@
+﻿namespace Exercices;
+
+public class SumTwoStrings
+{
+    public static string SumStrings(string a, string b)
+    {
+        string shortString;
+        string longString;
+
+        var result = "";
+        var retainer = 0;
+
+        a = a.TrimStart('0');
+        b = b.TrimStart('0');
+
+        if (a.Length > b.Length)
+        {
+            shortString = b;
+            longString = a;
+        }
+        else
+        {
+            shortString = a;
+            longString = b;
+
+        }
+
+        var delta = longString.Length - shortString.Length;
+        int i;
+        for (i = shortString.Length - 1; i >= 0; i--)
+        {
+            if (!int.TryParse(shortString[i].ToString(), out var value1))
+            {
+                return "-1";
+            }
+            if (!int.TryParse(longString[i + delta].ToString(), out var value2))
+            {
+                return "-1";
+            }
+
+            var sum = Convert.ToString(value1 + value2 + retainer);
+            retainer = sum.Length > 1 ? 1 : 0;
+
+            result = sum[^1] + result;
+        }
+
+        i = delta - 1;
+        while (i >= 0)
+        {
+            if (!int.TryParse(longString[i].ToString(), out var value))
+            {
+                return "-1";
+            }
+
+            var sum = Convert.ToString(value + retainer);
+            retainer = sum.Length > 1 ? 1 : 0;
+
+            result = sum[^1] + result;
+
+            i--;
+        }
+
+        if (retainer > 0)
+        {
+            result = retainer + result;
+        }
+
+        return result == "" ? "0" : result;
+    }
+}
