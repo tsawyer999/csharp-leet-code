1dd51ead34c926d00bdfb2da8a9f2e5753155103
name <email>
2023-09-27 16:41:21 -0400

    features: calculate the greater area

---
+++ Exercices.UnitTests/ContainerWithMostWaterTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercices.UnitTests;
+
+[TestClass]
+public class ContainerWithMostWaterTests
+{
+    [TestMethod]
+    public void WhenThereIsNoValuesReturnZero()
+    {
+        var calculator = new ContainerWithMostWater();
+        var values = new int[] { };
+
+        var result = calculator.Calculate(values);
+
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void CalculateHorizontalArea()
+    {
+        var calculator = new ContainerWithMostWater();
+        var values = new[] { 1, 1, 1, 1, 3, 1, 3, 1, 1, 1, 1 };
+        var area = calculator.Calculate(values);
+
+        Assert.AreEqual(11, area);
+    }
+
+    [TestMethod]
+    public void CalculateVerticalArea()
+    {
+        var calculator = new ContainerWithMostWater();
+        var values = new[] { 1, 3, 1, 3, 1 };
+        var area = calculator.Calculate(values);
+
+        Assert.AreEqual(9, area);
+    }
+
+
+    [TestMethod]
+    [DataRow(new[] {7, 1, 2, 3, 9}, 35)]
+    [DataRow(new[] {6, 9, 3, 4, 5, 8}, 40)]
+    public void CalculateRandomArea(int[] values, int expectedArea)
+    {
+        var calculator = new ContainerWithMostWater();
+        var area = calculator.Calculate(values);
+
+        Assert.AreEqual(expectedArea, area);
+    }
+
+}
---
+++ Exercices/ContainerWithMostWater.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercices;
+
+public class ContainerWithMostWater
+{
+    public int Calculate(int[] values)
+    {
+        if (values.Length == 0)
+        {
+            return 0;
+        }
+
+        var area = 0;
+        var limit = Math.Ceiling(values.Length / 2.0);
+        for (var leftIndex = 0; leftIndex < limit; leftIndex++)
+        {
+            for (var rightIndex = leftIndex + 1; rightIndex < values.Length; rightIndex++)
+            {
+                var leftValue = values[leftIndex];
+                var rightValue = values[rightIndex];
+
+                var width = rightIndex - leftIndex + 1;
+                var height = Math.Min(leftValue, rightValue);
+
+                var currentArea = width * height; 
+                if (currentArea > area)
+                {
+                    area = currentArea;
+                }
+            }
+        }
+
+        return area;
+    }
+}
