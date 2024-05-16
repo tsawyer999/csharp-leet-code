c38e203acc9721b13dc178b6ea9ef36b48ed5331
name <email>
2024-05-16 16:26:36 -0400

    feature: implement multithreading fizz-buzz challenge

---
+++ Exercices/ChallengeD/FizzBuzz.cs
@@ -0,0 +1,79 @@
+﻿namespace Exercices.ChallengeD
+{
+    public class FizzBuzz
+    {
+        private int n;
+        private volatile int i;
+        private object padlock = new();
+
+        public FizzBuzz(int n)
+        {
+            this.n = n;
+            this.i = 1;
+        }
+
+        public void Fizz(Action printFizz)
+        {
+            while (i <= n)
+            {
+                var ii = i;
+                lock (padlock)
+                {
+                    if (ii % 3 == 0 && ii % 5 != 0)
+                    {
+                        printFizz();
+                        i++;
+                    }
+                }
+            }
+        }
+
+        public void Buzz(Action printBuzz)
+        {
+            while (i <= n)
+            {
+                var ii = i;
+                lock (padlock)
+                {
+                    if (ii % 3 != 0 && ii % 5 == 0)
+                    {
+                        printBuzz();
+                        i++;
+                    }
+                }
+            }
+        }
+
+        public void Fizzbuzz(Action printFizzBuzz)
+        {
+            while (i <= n)
+            {
+                var ii = i;
+                lock (padlock)
+                {
+                    if (ii % 3 == 0 && ii % 5 == 0)
+                    {
+                        printFizzBuzz();
+                        i++;
+                    }
+                }
+            }
+        }
+
+        public void Number(Action<int> printNumber)
+        {
+            while (i <= n)
+            {
+                var ii = i;
+                lock (padlock)
+                {
+                    if (ii % 3 != 0 && ii % 5 != 0)
+                    {
+                        printNumber(ii);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exercices/ChallengeD/MultiThreadingFizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercices.ChallengeD
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Action printFizz = () =>
+            {
+                Console.WriteLine("Fizz");
+            };
+            Action printBuzz = () =>
+            {
+                Console.WriteLine("Buzz");
+            };
+            Action printFizzBuzz = () =>
+            {
+                Console.WriteLine("FizzBuzz");
+            };
+            Action<int> printNumber = (int value) =>
+            {
+                Console.WriteLine(value);
+            };
+
+            var fizzBuzz = new FizzBuzz(15);
+
+            Task.WaitAll([
+                Task.Run(() =>
+                {
+                    fizzBuzz.Fizz(printFizz);
+                }),
+                Task.Run(() =>
+                {
+                fizzBuzz.Buzz(printBuzz);
+                }),
+                Task.Run(() =>
+                {
+                fizzBuzz.Fizzbuzz(printFizzBuzz);
+                }),
+                Task.Run(() =>
+                {
+                fizzBuzz.Number(printNumber);
+                })
+            ]);
+        }
+    }
+}
