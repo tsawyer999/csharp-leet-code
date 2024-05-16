b79341991ef884e6e2c786c500dd137c05424921
name <email>
2024-05-16 16:11:41 -0400

    feature: implement longuest substring challenge

---
+++ Exercices/Challenge04/LonguestSubstring.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercices.Challenge04
+{
+    public class LonguestSubstring
+    {
+        public int Process(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return 0;
+            }
+
+            var longuestLength = 0;
+            var letters = new List<char>();
+
+            foreach (var letter in value)
+            {
+                if (letters.Contains(letter))
+                {
+                    if (letters.Count > longuestLength)
+                    {
+                        longuestLength = letters.Count;
+                    }
+                    letters.Clear();
+                }
+                else
+                {
+                    letters.Add(letter);
+                }
+
+            }
+            if (letters.Count > longuestLength)
+            {
+                longuestLength = letters.Count;
+            }
+
+            return longuestLength;
+        }
+    }
+}
---
+++ Exercices/Challenge04/LonguestSubstringTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
+
+namespace Exercices.Challenge04
+{
+    [TestClass]
+    public class LonguestSubstringTests
+    {
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        public void Return0WhenStringIsNullOrEmpty(string value)
+        {
+            var processor = new LonguestSubstring();
+
+            var result = processor.Process(value);
+
+            result.Should().Be(0);
+        }
+
+        [TestMethod]
+        [DataRow("abc", 3)]
+        [DataRow("abcc", 3)]
+        [DataRow("abccmnopqr", 6)]
+        public void ReturnSubstringLength(string value, int expectedLength)
+        {
+            var processor = new LonguestSubstring();
+
+            var result = processor.Process(value);
+
+            result.Should().Be(expectedLength);
+        }
+    }
+}
