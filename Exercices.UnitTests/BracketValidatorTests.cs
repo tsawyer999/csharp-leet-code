fc886538089f7d4eda8b67dc716c69ce5ed81d9f
name <email>
2024-05-16 14:34:25 -0400

    feature: implement brackets problem

---
+++ Exercices.UnitTests/BracketValidatorTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Exercices.UnitTests
+{
+    [TestClass]
+    public class BracketValidatorTests
+    {
+        [TestMethod]
+        public void ReturnFalseWhenInputIsNull()
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void ReturnTrueWhenInputIsEmptyOrSpace(string input)
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("()")]
+        [DataRow("[]")]
+        [DataRow("{}")]
+        public void ReturnTrueWhenInputIsClosed(string input)
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("(")]
+        [DataRow("[")]
+        [DataRow("{")]
+        public void ReturnFalseWhenInputIsOpen(string input)
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(input);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [DataRow("([{}])")]
+        [DataRow("[]{}()")]
+        [DataRow("(((())))")]
+        public void ReturnTrueWhenNestedIsClosed(string input)
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("({)}")]
+        [DataRow("[{}()")]
+        [DataRow("(((()))")]
+        public void ReturnFalseWhenNestedIsOpened(string input)
+        {
+            var validator = new BracketValidator();
+            var result = validator.IsValid(input);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Exercices/BracketValidator.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercices
+{
+    public class BracketValidator
+    {
+        private const string OpeningBrackets = "[({";
+        private const string ClosingBrackets = "])}";
+
+        private readonly Dictionary<char, char> MatchingBrackets = new()
+        {
+            { '[', ']' },
+            { '{', '}' },
+            { '(', ')' }
+        };
+
+        public bool IsValid(string? input)
+        {
+            var stack = new Stack<char>();
+
+            if (input == null)
+            {
+                return false;
+            }
+
+            foreach (var letter in input)
+            {
+                if (OpeningBrackets.Contains(letter))
+                {
+                    stack.Push(letter);
+                }
+                else if (ClosingBrackets.Contains(letter))
+                {
+                    var openingBracket = stack.Pop();
+                    var expectedClosingBracket = MatchingBrackets[openingBracket];
+
+                    if (letter != expectedClosingBracket)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return (stack.Count == 0);
+        }
+    }
+}
