068c4c3f0a2753c70882cd7b262e221b30faa109
name <email>
2024-05-17 12:50:47 -0400

    feature: implement a linked list reversal

---
+++ Exercices/Challenge07/LinkedListReversal.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercices.Challenge07;
+
+public class LinkedListReversal
+{
+    public Node? SubReverseList(Node root)
+    {
+        var previousNode = null as Node;
+        var node = root;
+        Node? nextNode;
+
+        while (node != null)
+        {
+            nextNode = node.NextNode;
+            node.NextNode = previousNode;
+            previousNode = node;
+            node = nextNode;
+        }
+
+        return previousNode;
+    }
+}
+
+public class Node(int value)
+{
+    public int Value { get; set; } = value;
+    public Node? NextNode { get; set; }
+
+    public override string ToString()
+    {
+        return NextNode != null
+            ? Value + NextNode.ToString()
+            : Value.ToString();
+    }
+}
---
+++ Exercices/Challenge07/LinkedListReversalTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace Exercices.Challenge07;
+
+[TestClass]
+public class LinkedListReversalTests
+{
+    [TestMethod]
+    public void Return()
+    {
+        var firstNode = new Node(1);
+        var node = firstNode;
+        for (var i = 2; i <= 5; i++)
+        {
+            node.NextNode = new Node(i);
+            node = node.NextNode;
+        }
+
+        var processor = new LinkedListReversal();
+
+        var result = processor.SubReverseList(firstNode);
+
+        Assert.AreEqual("54321", result?.ToString());
+    }
+}
