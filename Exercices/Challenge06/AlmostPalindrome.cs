03b59db704bcdeb338a448dcbc61b594ae575e8d
name <email>
2024-05-16 21:39:45 -0400

    feature: implement almost palindrome challenge

---
+++ Exercices/Challenge06/AlmostPalindrome.cs
@@ -0,0 +1,88 @@
+﻿using Exercices.Challenge05;
+
+namespace Exercices.Challenge06;
+
+public class AlmostPalindrome
+{
+    public bool IsAlmostPalindrome(string value, int level = 1)
+    {
+        if (level > 2)
+        {
+            return false;
+        }
+
+        var leftIndex = 0;
+        var rightIndex = value.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            while (!IsAlpha(value[leftIndex]))
+            {
+                leftIndex++;
+            }
+
+            while (!IsAlpha(value[rightIndex]))
+            {
+                rightIndex--;
+            }
+
+            if (value[leftIndex] != value[rightIndex])
+            {
+                if (!IsSameChar(value[leftIndex], value[rightIndex]))
+                {
+                    var length = rightIndex - leftIndex;
+                    var substring = value.Substring(leftIndex, length);
+                    if (IsAlmostPalindrome(substring, level + 1))
+                    {
+                        return true;
+                    }
+
+                    substring = value.Substring(leftIndex + 1, length);
+                    if (IsAlmostPalindrome(substring, level + 1))
+                    {
+                        return true;
+                    }
+
+
+                    return false;
+                }
+            }
+
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return true;
+    }
+
+    private bool IsAlpha(char c)
+    {
+        if (c < 65)
+        {
+            return false;
+        }
+
+        if (c <= 90)
+        {
+            return true;
+        }
+
+        if (c < 97)
+        {
+            return false;
+        }
+
+        if (c <= 122)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsSameChar(char l, char r)
+    {
+        var diff = l - r;
+        return diff is 32 or -32;
+    }
+}
---
+++ Exercices/Challenge06/AlmostPalindromeTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace Exercices.Challenge06;
+
+[TestClass]
+public class AlmostPalindromeTests
+{
+    [TestMethod]
+    [DataRow("ZZZabacZZZ")]
+    [DataRow("ZZZcabaZZZ")]
+    [DataRow("ZZZcabbaZZZ")]
+    public void ReturnTrueWhenAlmostPalindrome(string value)
+    {
+        var processor = new AlmostPalindrome();
+
+        var result = processor.IsAlmostPalindrome(value);
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    [DataRow("ababb")]
+    public void ReturnFalseWhenAlmostPalindrome(string value)
+    {
+        var processor = new AlmostPalindrome();
+
+        var result = processor.IsAlmostPalindrome(value);
+
+        result.Should().BeFalse();
+    }
+}
